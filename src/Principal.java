196a5e12b9746c8d7cf1467f0dc6eb789f77bd4b
João Silva <llpublio@sga.pucminas.br>
2024-04-11 09:31:17 -0300

    Update Principal.java
    Primeira atualização - Encontrar Articulações

--- src/Principal.java
+++ src/Principal.java
@@ -3,12 +3,13 @@ import java.util.*;
 class Grafo {
     private int V;
     private ArrayList<Integer>[] adjacente;
+    Set<Integer> articulacoes = new HashSet<>();
 
     Grafo(int v) {
         V = v;
         adjacente = new ArrayList[v];
         for (int i = 0; i < v; ++i)
-        adjacente[i] = new ArrayList<Integer>();
+            adjacente[i] = new ArrayList<Integer>();
     }
 
     void adicionaAresta(int v, int w) {
@@ -28,37 +29,71 @@ class Grafo {
     }
 
     List<List<Integer>> encontrarBlocos() {
-        
-        // Inicializações
         boolean visitado[] = new boolean[V];
         int desc[] = new int[V];
         int baixo[] = new int[V];
         int pai[] = new int[V];
         List<List<Integer>> blocos = new ArrayList<>();
-    
-        // Marca todos os vértices como não visitados e inicializa os arrays desc e baixo
+
         for (int i = 0; i < V; ++i) {
-            pai[i] = -1; // Nenhum vértice possui pai inicialmente
-            visitado[i] = false; // Todos os vértices são marcados como não visitados
+            pai[i] = -1;
+            visitado[i] = false;
         }
-    
-        // Para cada vértice no grafo
+
         for (int i = 0; i < V; ++i) {
-            // Se o vértice não foi visitado ainda
             if (!visitado[i]) {
-                // Cria uma lista para armazenar os vértices do bloco
                 List<Integer> bloco = new ArrayList<>();
-                // Chama a função DFS para explorar o bloco conectado ao vértice atual
                 DFS(i, visitado, -1, baixo, desc, bloco);
-                // Adiciona o bloco encontrado à lista de blocos
                 blocos.add(bloco);
             }
         }
-    
-        // Retorna a lista de blocos encontrados
+
         return blocos;
     }
-    
+
+    void encontrarArticulacoes() {
+        boolean visitado[] = new boolean[V];
+        int desc[] = new int[V];
+        int baixo[] = new int[V];
+        int pai[] = new int[V];
+        int tempo = 0;
+
+        for (int i = 0; i < V; ++i) {
+            pai[i] = -1;
+            visitado[i] = false;
+        }
+
+        for (int i = 0; i < V; ++i) {
+            if (!visitado[i]) {
+                encontrarArticulacoesUtil(i, visitado, pai, baixo, desc, tempo);
+            }
+        }
+    }
+
+    void encontrarArticulacoesUtil(int u, boolean visitado[], int pai[], int baixo[], int desc[], int tempo) {
+        int filhos = 0;
+        visitado[u] = true;
+        desc[u] = baixo[u] = ++tempo;
+
+        for (Integer v : adjacente[u]) {
+            if (!visitado[v]) {
+                filhos++;
+                pai[v] = u;
+                encontrarArticulacoesUtil(v, visitado, pai, baixo, desc, tempo);
+
+                baixo[u] = Math.min(baixo[u], baixo[v]);
+
+                if (pai[u] == -1 && filhos > 1) {
+                    articulacoes.add(u);
+                }
+                if (pai[u] != -1 && baixo[v] >= desc[u]) {
+                    articulacoes.add(u);
+                }
+            } else if (v != pai[u]) {
+                baixo[u] = Math.min(baixo[u], desc[v]);
+            }
+        }
+    }
 }
 
 public class Principal {
@@ -81,39 +116,30 @@ public class Principal {
 
         long startTime = System.nanoTime();
         List<List<Integer>> blocos = g.encontrarBlocos();
+        g.encontrarArticulacoes();
         long endTime = System.nanoTime();
 
         imprimirBlocos(blocos);
+        System.out.println("Articulações encontradas: " + g.articulacoes);
         long duration = (endTime - startTime);
         System.out.println("Tempo de execução: " + duration + " nanossegundos");
     }
 
-    // Método para imprimir os blocos
     public static void imprimirBlocos(List<List<Integer>> blocos) {
-
-        // Loop sobre cada bloco na lista de blocos
         for (int i = 0; i < blocos.size(); i++) {
-            // Obtém a lista de vértices do bloco atual
             List<Integer> bloco = blocos.get(i);
-        
-            // Imprime informações sobre o bloco
             System.out.print("Bloco " + (i + 1) + ": composto");
 
-            // Verifica se o bloco contém apenas um vértice
             if (bloco.size() == 1) {
-                // Se sim, imprime uma mensagem indicando que o bloco possui apenas um vértice
                 System.out.println(" apenas pelo vértice " + bloco.get(0) + ".");
             } else {
-                // Se não, imprime uma mensagem indicando os vértices que compõem o bloco
                 System.out.print(" pelos vértices ");
                 for (int j = 0; j < bloco.size(); j++) {
                     System.out.print(bloco.get(j));
-                    // Se não for o último vértice do bloco, imprime um traço como separador
                     if (j < bloco.size() - 1) {
                         System.out.print(" - ");
                     }
                 }
-                // Imprime uma nova linha para finalizar a mensagem do bloco
                 System.out.println(".");
             }
         }
