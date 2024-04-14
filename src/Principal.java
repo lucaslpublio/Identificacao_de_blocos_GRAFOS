b6bc8be20ce818d14a27168845d911e1eca0e890
João Silva <llpublio@sga.pucminas.br>
2024-04-14 18:27:52 -0300

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.TreeSet;
 import java.util.Comparator;
 import java.util.Set;
+import java.util.Random;
 
 class Grafo {
     private int V; // Numero de vertices
@@ -14,12 +15,70 @@ class Grafo {
         adj = new ArrayList<>(V);
         for (int i = 0; i < V; ++i)
             adj.add(new ArrayList<>());
+    
+        // Adiciona arestas aleatorias de forma mais esparsa
+        Random rand = new Random();
+        for (int i = 1; i < V; i++) {
+            int vertice = rand.nextInt(i);
+            adicionaAresta(i, vertice);
+        }
+        for (int i = 0; i < V; i++) {
+            int numArestas = rand.nextInt(2); // Defina o numero maximo de arestas por vertice (por exemplo, 2)
+            for (int j = 0; j < numArestas; j++) {
+                int vertice = rand.nextInt(V);
+                if (vertice != i && !adj.get(i).contains(vertice)) {
+                    adicionaAresta(i, vertice);
+                }
+            }
+        }
+    }
+    
+    Grafo(int v, int w) {
+        V = v;
+        adj = new ArrayList<>(V);
+        for (int i = 0; i < w; ++i)
+            adj.add(new ArrayList<>());
     }
 
     // Funcao para adicionar uma aresta ao grafo
     void adicionaAresta(int v, int w) {
         adj.get(v).add(w);
         adj.get(w).add(v);
+    }
+    
+    // Funcao para remover um vertice do grafo
+    void removerVertice(int v) {
+        adj.remove(v);
+        for(int i = 0; i < adj.size(); i++){
+            for(int j = 0; j < adj.get(i).size(); j++){
+                if(adj.get(i).get(j) == v){
+                    adj.get(i).remove(j);
+                }
+            }
+        }
+        V--;
+        for(int i = 0; i < adj.size(); i++){
+            for(int j = 0; j < adj.get(i).size(); j++){
+                if(adj.get(i).get(j) > v){
+                    int aux = adj.get(i).get(j);
+                    adj.get(i).set(j, aux-1);
+                }
+            }
+        }
+    }
+    
+    // Funcao para criar uma copia do Grafo
+    public Grafo copiarGrafo() {
+        Grafo novoGrafo = new Grafo(V, V);
+        for(int i = 0; i < V; i++){
+            for(int j = 0; j < adj.get(i).size(); j++){
+                int u = adj.get(i).get(j);
+                if(u >= i){
+                    novoGrafo.adicionaAresta(i, u);
+                }
+            }
+        }
+        return novoGrafo;
     }
 
     // Funcao de utilidade para verificar se existe um ciclo
@@ -95,8 +154,46 @@ class Grafo {
         return sum;
     }
     
+    //Funcado dfs sem alteracoes
+    private void dfsOriginal(int vertice, Set<Integer> visitados) {
+        visitados.add(vertice);
+        for (int vizinho : adj.get(vertice)) {
+            if (!visitados.contains(vizinho)) {
+                dfsOriginal(vizinho, visitados);
+            }
+        }
+    }
+    
+    // Funcao para encontrar a quantidade de componentes conexos do grafo
+    public int componentesConexos(){
+        int componentes = 0;
+        Set<Integer> visitados = new HashSet<>();
+
+        for (int vertice = 0; vertice < V; vertice++) {
+            if (!visitados.contains(vertice)) {
+                dfsOriginal(vertice, visitados);
+                componentes++;
+            }
+        }
+        return componentes;
+    } 
+    
+    // Funcao para encontrar articulacoes a partir dos componentes conexos
+    public ArrayList<Integer> encontrarArticulacoes(){
+        ArrayList<Integer> articulacoes = new ArrayList<>();
+        for(int i = 0; i < V; i++){
+            Grafo aux = this.copiarGrafo();
+            aux.removerVertice(i);
+            if(aux.componentesConexos() > this.componentesConexos()){
+                articulacoes.add(i);
+            }
+        }
+        
+        return articulacoes;
+    }
+    
     // Funcao que encontra articulacoes
-    void findArticulations() {
+    void findArticulationsTarjan() {
         boolean[] visited = new boolean[V];
         int[] disc = new int[V]; // Tempo de descoberta
         int[] low = new int[V]; // Menor tempo de descoberta alcancavel
@@ -105,7 +202,7 @@ class Grafo {
 
         Arrays.fill(parent, -1);
 
-        ArrayList<Integer> articulacoes = new ArrayList<>(); // Lista para armazenar as articulações
+        ArrayList<Integer> articulacoes = new ArrayList<>(); // Lista para armazenar as articulacoes
 
         for (int i = 0; i < V; i++) {
             if (!visited[i]) {
@@ -113,26 +210,26 @@ class Grafo {
             }
         }
 
-        // Impressão das articulações encontradas
-        System.out.println("Articulações encontradas:");
+        // Impressao das articulacoes encontradas
+        System.out.println("Articulacoes encontradas:");
         for (int articulacao : articulacoes) {
             System.out.println(articulacao);
         }
 
-        // Verificar e imprimir blocos de articulações adjacentes
+        // Verificar e imprimir blocos de articulacoes adjacentes
         verificarBlocosDeArticulacoes(articulacoes);
     }
 
-    // Método para verificar e imprimir blocos de articulações adjacentes
+    // Metodo para verificar e imprimir blocos de articulacoes adjacentes
     void verificarBlocosDeArticulacoes(ArrayList<Integer> articulacoes) {
+        System.out.println("Blocos formados por apenas 2 vertices:");
         for (int i = 0; i < articulacoes.size() - 1; i++) {
             int art1 = articulacoes.get(i);
             for (int j = i + 1; j < articulacoes.size(); j++) {
                 int art2 = articulacoes.get(j);
-                // Verifica se as articulações são adjacentes
+                // Verifica se as articulacoes sao adjacentes
                 if (adj.get(art1).contains(art2)) {
-                    System.out.println("Bloco de articulações adjacentes encontradas:");
-                    System.out.println(art1 + " - " + art2);
+                    System.out.println( "[" + art1 + " , " + art2 + "]");
                 }
             }
         }
@@ -152,9 +249,9 @@ class Grafo {
 
                 low[u] = Math.min(low[u], low[v]);
 
-                // Verifica se u é uma articulacao
+                // Verifica se u e uma articulacao
                 if ((parent[u] == -1 && children > 1) || (parent[u] != -1 && low[v] >= disc[u])) {
-                    articulacoes.add(u); // Adiciona a articulação encontrada à lista
+                    articulacoes.add(u); // Adiciona a articulacao encontrada a lista
                 }
             } else if (v != parent[u]) {
                 low[u] = Math.min(low[u], disc[v]);
@@ -186,9 +283,10 @@ class Grafo {
        return blocos;
     }
     
-    // Método para encontrar e imprimir blocos de articulações adjacentes
+    // Metodo para encontrar e imprimir blocos de articulacoes adjacentes
     void encontraBlocosDeArticulacoes() {
         ArrayList<Integer> articulacoes = new ArrayList<>();
+        
         for (int i = 0; i < V; i++) {
             if (adj.get(i).size() >= 2) {
                 articulacoes.add(i);
@@ -199,36 +297,25 @@ class Grafo {
             int art1 = articulacoes.get(0);
             int art2 = articulacoes.get(1);
             
-            // Verifica se as duas articulações são adjacentes
+            // Verifica se as duas articulacos sao adjacentes
             if (adj.get(art1).contains(art2)) {
-                System.out.println("Blocos de articulações adjacentes encontrados:");
-                System.out.println("Articulação 1: " + art1);
-                System.out.println("Articulação 2: " + art2);
+                System.out.println("Articulacao 1: " + art1);
             }
         }
     }
 
     // Metodo de teste
     public static void main(String args[]) {
-        Grafo g = new Grafo(9);
-        g.adicionaAresta(0, 1);
-        g.adicionaAresta(1, 2);
-        g.adicionaAresta(1, 3);
-        g.adicionaAresta(2, 3);
-        g.adicionaAresta(3, 0);
-        g.adicionaAresta(0, 4);
-        g.adicionaAresta(4, 5);
-        g.adicionaAresta(5, 6);
-        g.adicionaAresta(6, 7);
-        g.adicionaAresta(7, 8);
-        g.adicionaAresta(8, 5);
+        Grafo g = new Grafo(10);
 
         // Encontra e imprime os ciclos e blocos
         ArrayList<ArrayList<Integer>> ciclos = g.encontraCiclos();
         ArrayList<ArrayList<Integer>> blocos = g.blocos();
         if (ciclos.isEmpty()) {
+            System.out.println("Ex 1 : Encontrando blocos atraves dos ciclos");
             System.out.println("O grafo nao contem ciclos.");
         } else {
+            System.out.println("Ex 1 : Encontrando blocos atraves dos ciclos");
             System.out.println("Ciclos encontrados:");
             for (ArrayList<Integer> ciclo : ciclos) {
                 System.out.println(ciclo);
@@ -239,10 +326,23 @@ class Grafo {
             }
         }
 
-        // Encontra e imprime os pontos de articulacao
-        g.findArticulations();
+        System.out.println("\nEx 2: Testando Conectividade com remocao e insercao de vertices");
+        // Encontra articulacoes usando o metodo de verificar os componentes conexos
+        System.out.println("Articulacoes: "+ g.encontrarArticulacoes());
 
-        // Encontra e imprime os blocos de articulações adjacentes
+        System.out.println("\nEx 3: Tarjan");
+
+        System.out.println("Blocos encontrados:");
+        for (ArrayList<Integer> bloco : blocos) {
+            System.out.println(bloco);
+        }
+
+        // Encontra e imprime os blocos de articulacoes adjacentes
         g.encontraBlocosDeArticulacoes();
+
+        // Encontra e imprime os pontos de articulacao usando Tarjan
+        g.findArticulationsTarjan();
+        
+       
     }
 }
